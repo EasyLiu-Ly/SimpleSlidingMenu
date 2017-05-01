368859d8cdfc48fb1174f3c49b40b0e3931c5cc1
EasyLiu <ly_eyl@163.com>
2017-05-01 15:00:56 +0800

    add project

---
+++ SimpleSlidingMenu/app/src/main/java/com/easyliu/simpleslidingmenu/MainActivity.java
@@ -0,0 +1,44 @@
+package com.easyliu.simpleslidingmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.easyliu.simpleslidingmenu.dummy.DummyContent;
+
+public class MainActivity extends AppCompatActivity
+    implements ItemFragment.OnListFragmentInteractionListener {
+  private SlidingMenuLayout mSlideMenuLayout;
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mSlideMenuLayout = new SlidingMenuLayout(this);
+    setContentView(mSlideMenuLayout);
+    initSlideMenuLayout();
+  }
+
+  private void initSlideMenuLayout() {
+    getSupportFragmentManager().beginTransaction()
+        .replace(SlidingMenuLayout.LEFT_TAG, ItemFragment.newInstance(10))
+        .commit();
+    getSupportFragmentManager().beginTransaction()
+        .replace(SlidingMenuLayout.MIDDLE_TAG, ItemFragment.newInstance(20))
+        .commit();
+    getSupportFragmentManager().beginTransaction()
+        .replace(SlidingMenuLayout.RIGHT_TAG, ItemFragment.newInstance(30))
+        .commit();
+    mSlideMenuLayout.setMenuMode(SlidingMenuLayout.MenuMode.LEFT);
+    mSlideMenuLayout.setSlidingMode(SlidingMenuLayout.SlidingMode.EDGE);
+    mSlideMenuLayout.setSlideEnable(true);
+  }
+
+  @Override public void onBackPressed() {
+    if (mSlideMenuLayout.isMenuShowing()) {
+      mSlideMenuLayout.closeMenu();
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+  }
+}
